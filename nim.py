0df56afbdf1ee9d766ca8c1c6fe6720675fbc555
barbara7777 <badobe.lipnik@gmail.com>
2022-05-19 21:25:37 +0200

    code reformatting

--- nim.py
+++ nim.py
@@ -1,5 +1,6 @@
 import random
 
+
 def generate_piles(n, m):
     "Generates random number of piles (between 1 and n) each with random number of coins (between 1 and m)"
     game = []
@@ -10,41 +11,36 @@ def generate_piles(n, m):
     return game
 
 
-
 class Nim():
 
     def __init__(self, initial):
-        "Initialize game"
-        "piles .... list of numbers - each number represents # of coins in each pile"
-        "player ... 0 or 1"
-        "winner ... 0 or 1 or -1 (-1 means no winner yet)"
+        """
+        Initialize game
+        piles .... list of numbers - each number represents # of coins in each pile
+        player ... 0 or 1
+        winner ... 0 or 1 or -1 (-1 means no winner yet)"""
         self.piles = initial.copy()
         self.player = 0
         self.winner = -1
 
-
-
     def possible_actions(self):
-        "Returns all possible actions for a given set of piles"
-        "action (i, j) means take j coins from i-th pile"        
+        """
+        Returns all possible actions for a given set of piles
+        action (i, j) means take j coins from i-th pile"""
         actions = []
         for i in range(len(self.piles)):
-            for j in range(1, self.piles[i]): # player has to take at least one coin
+            for j in range(1, self.piles[i]):  # player has to take at least one coin
                 actions.append((i, j))
         return actions
 
-
-
     def switch_player(self):
-        "Switches player"
+        """Switches player"""
         self.player = 0 if self.player == 1 else 1
 
-
-
     def check_for_winner(self):
-        "Checks for winner - returns True if winner found, else it returns False"
-        "First player makes a move, then we switch player, then we check for winner"
-        "If there are no piles left that means other player took the last one and the player that is on the move won"
+        """Checks for winner - returns True if winner found, else it returns False
+        First player makes a move, then we switch player, then we check for winner
+        If there are no piles left that means other player took the last one and the player that is on the move won"""
 
         counter = 0
         for pile in self.piles:
@@ -55,17 +51,12 @@ class Nim():
             return True
         return False
 
-
-
     def make_move(self, action):
-        "Makes move and switches player"
-
+        """Makes move and switches player"""
         if self.winner == -1:
-            (i, j) = action # take j coins from i-th pile
+            (i, j) = action  # take j coins from i-th pile
             self.piles[i] -= j
             self.switch_player()
             self.check_for_winner()
         else:
             raise Exception("Winner already determined")
-
-
